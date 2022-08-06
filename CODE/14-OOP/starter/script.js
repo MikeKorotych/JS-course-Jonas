86475f314e5340f3b1aa5fc6cac453438ab4303c
Mike Korotych <mixakievchanen@gmail.com>
2022-08-06 06:46:57 +0300

    new section

--- CODE/14-OOP/starter/script.js
+++ CODE/14-OOP/starter/script.js
@@ -190,30 +190,30 @@
 // };
 
 // Coding challenge #2
-// class CarCl {
-//   constructor(make, speed) {
-//     this.make = make;
-//     this.speed = speed;
-//   }
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
 
-//   accelerate() {
-//     this.speed += 10;
-//   }
+  accelerate() {
+    this.speed += 10;
+  }
 
-//   brake() {
-//     this.speed -= 5;
-//   }
+  brake() {
+    this.speed -= 5;
+  }
 
-//   get speedUS() {
-//     return this.speed / 1.6;
-//   }
+  get speedUS() {
+    return this.speed / 1.6;
+  }
 
-//   set speedUS(speed) {
-//     this.speed = speed * 1.6;
-//   }
-// }
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
 
-// const ford = new CarCl('Ford', 120);
+const ford = new CarCl('Ford', 120);
 
 // console.log(ford);
 // console.log(ford.speedUS);
@@ -414,3 +414,162 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
+
+/////////////////////////////////////////////////////////////
+// 1) Public fields
+// 2) Private fields
+
+// 3) Public methods
+// 4) Private methods
+
+class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+
+  // 2) Private fields (instances)
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    // protected property
+    this.#pin = pin;
+    // this.#movements = [];
+    // this.locale = navigator.language;
+
+    console.log(`Thank's for openning an account ${this.owner}`);
+  }
+
+  // 3) Public methods
+  //Public interface
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(val) {
+    this.#movements.push(val);
+    return this;
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+    return this;
+  }
+
+  requestLoan(val) {
+    // if (this.#approveLoan(val)) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log(
+        `Loan approved: ${this.#movements.slice(-1)} ${this.currency}`
+      );
+      return this;
+    }
+  }
+
+  // static будет доступен только в классе, но не на экземплярах
+  static helper() {
+    console.log('Helper');
+  }
+
+  // 4) Private methods
+  // #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
+  }
+}
+
+const acc1 = new Account('Mike', 'UAH', 1111);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());
+
+console.log(acc1);
+Account.helper();
+
+// console.log(acc1.#pin);
+// console.log(acc1.#movements);
+// console.log(acc1.#approveLoan(100));
+
+// Chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(acc1.getMovements());
+//////////////////////////////////////////////////////////
+
+// Coding Challenge #4
+// Your tasks:
+// 1. Re-create Challenge #3, but this time using ES6 classes: create an 'EVCl'
+// child class of the 'CarCl' class
+// 2. Make the 'charge' property private
+// 3. Implement the ability to chain the 'accelerate' and 'chargeBattery'
+// methods of this class, and also update the 'brake' method in the 'CarCl'
+// class. Then experiment with chaining!
+// Test data:
+// § Data car 1: 'Rivian' going at 120 km/h, with a charge of 23
+
+class EVCL extends CarCl {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} is going at ${this.speed} km/h with a charge of ${
+        this.#charge
+      }%`
+    );
+    return this;
+  }
+
+  brake() {
+    this.speed -= 5;
+    return this;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+}
+
+const rivian = new EVCL('Rivian', 120, 23);
+console.log(rivian);
+rivian
+  .accelerate()
+  .brake()
+  .accelerate()
+  .accelerate()
+  .chargeBattery(50)
+  .accelerate();
+
+console.log(rivian.speedUS);
+
+/// Example from coding challennge #3 ///
+
+// const EV = function (make, speed, charge) {
+//   Car.call(this, make, speed);
+//   this.charge = charge;
+// };
+
+// // Car.prototype = Object.create(EV.prototype);
+// EV.prototype = Object.create(Car.prototype);
+
+// EV.prototype.chargeBattery = function (chargeTo) {
+//   this.charge = chargeTo;
+// };
+
+// EV.prototype.accelerate = function () {
+//   this.speed += 20;
+//   this.charge--;
+//   console.log(
+//     `${this.make} is going at ${this.speed} km/h with a charge of ${this.charge}%`
+//   );
+// };
+
+// const tesla = new EV('Tesla', 120, 23);
